b1f0fb3cf1bfd368816a4882b3b0e4082d9de5e3
aunn <aunn_it08@hotmail.com>
2018-06-16 23:54:16 +0700

    add buefy

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import Buefy from 'buefy'
+import 'buefy/lib/buefy.css'
 
 Vue.config.productionTip = false
-
+Vue.use(Buefy)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
