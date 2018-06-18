da79e37e7ac6630000cc1417c86c22599530639a
aunn <aunn_it08@hotmail.com>
2018-06-18 20:09:33 +0700

    add simple style

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>
-    My awesome todo list
-  </div>
+  <section class="hero">
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <div>x Buy food</div>
+        <div>x Learn Bulma</div>
+      </div>
+    </div>
+  </section>
 </template>
